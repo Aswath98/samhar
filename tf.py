fc7b20850d5f575dc8c56505db24360618bd59ce
Aswath Rao V S <46022653+Aswath98@users.noreply.github.com>
2020-05-08 18:40:19 +0530

    Add files via upload

---
+++ tf.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv
+
+# Load an color image in grayscale
+img = cv.imread('pooja-2.jpg',0)
---
+++ untitled0.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 19:04:32 2018
+
+@author: USER
+"""
+
+import cv2
